e886248129cc947d79a53b34be7b583406021c2c
Zamir31 <noriegad258@gmail.com>
2023-02-23 17:31:04 -0600

    terminado la calculadora

---
+++ App_Prueba/App_Prueba/Models/Expresion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_Prueba.Models
+{
+    public class Expresion
+    {
+        public int num1  { get; set; }
+
+        public int num2 { get; set; }
+
+        public int resultado { get; set; }
+
+
+        public string sumar()
+        {
+            resultado = num1 + num2;
+            return resultado.ToString();
+        }
+
+        public string restar()
+        {
+            resultado = num1 - num2;
+            return resultado.ToString();
+        }
+    }
+}
---
+++ App_Prueba/App_Prueba/viewModels/viewModelExpresion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using App_Prueba.Models;
+
+namespace App_Prueba.viewModels
+{
+    public class viewModelExpresion : INotifyPropertyChanged
+    {
+        private int num1;
+        private int num2;
+        private string msj;
+
+        public viewModelExpresion() {
+            sumar = new Command(() =>
+            {
+                Expresion expresion = new Expresion()
+                {
+                    num1 = this.num1,
+                    num2 = this.num2,
+                };
+                Msj = expresion.sumar();
+            });
+
+            restar = new Command(() =>
+            {
+                Expresion expresion = new Expresion()
+                {
+                    num1 = this.num1,
+                    num2 = this.num2,
+                };
+                Msj = expresion.restar();
+            });
+        }
+
+
+        public string Msj
+        {
+            get => msj;
+            set
+            {
+                msj = value;
+                var argsEvent = new PropertyChangedEventArgs(nameof(Msj));
+                PropertyChanged?.Invoke(this, argsEvent);
+            }
+        }
+        public int Num1
+        {
+            get => num1;
+            set
+            {
+                num1 = value;
+                var argsEvent = new PropertyChangedEventArgs(nameof(Num1));
+                PropertyChanged?.Invoke(this, argsEvent);
+            }
+        }
+
+        public int Num2
+        {
+            get => num2;
+            set
+            {
+                num2 = value;
+                var argsEvent = new PropertyChangedEventArgs(nameof(Num2));
+                PropertyChanged?.Invoke(this, argsEvent);
+            }
+        }
+
+        public Command sumar { get; }
+        public Command restar { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
